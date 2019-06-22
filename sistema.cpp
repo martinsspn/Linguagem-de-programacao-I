cdf7dd25ffa199bf8a57cdc8cd580dfc80bf82d9
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-22 08:38:52 -0300

    Update sistema.cpp

--- sistema.cpp
+++ sistema.cpp
@@ -97,9 +97,3 @@ void Sistema::operarSistema(){
 Sistema::~Sistema(){
 
 }
-
-//Criar arquivo caso não exista
-//ler de um arquivo existente
-//--salvar em um map 
-//imprimir em um arquivo
-//--imprimir de um map
