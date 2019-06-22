1f940a8cab517d744d39c9390542993793891516
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-22 08:37:53 -0300

    Create manipular_arquivos.h

---
+++ manipular_arquivos.h
@@ -0,0 +1,10 @@
+#ifndef ARQUIVOS_H
+#define ARQUIVOS_H
+
+/**@brief classe referente à manipulação dos arquivos, ler e escrever nos arquivos*/
+class Manipulando_arquivos:
+      private:
+      
+      public:
+
+#endif
