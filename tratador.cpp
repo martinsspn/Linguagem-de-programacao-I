ff55786ce96b1043910fb1c18cb8e92c198baa0a
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-22 16:46:00 -0300

    Update tratador.cpp

--- tratador.cpp
+++ tratador.cpp
@@ -1,7 +1,7 @@
 #include <tratador.h>
 
-Tratador::Tratador(int m_nivel_de_segurança){
-	this->m_nivel_de_segurança = m_nivel_de_segurança;
+Tratador::Tratador(int m_nivel_de_seguranca) : Funcionario(int id, string nome, string cpf, short idade, short tipo_sanguineo, char fator_rh, string especialidade) {
+	this->m_nivel_de_seguranca = m_nivel_de_seguranca;
 };
 
 void Tratador::setM_nivel_de_segurança(int m_nivel_de_segurança){
