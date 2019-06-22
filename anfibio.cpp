a7d85315a0d0425aa1917dbfdc6955e21fae9d40
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-22 16:33:44 -0300

    Update anfibio.cpp

--- anfibio.cpp
+++ anfibio.cpp
@@ -1,6 +1,7 @@
 #include <anfibio.h>
 
 Anfibio::Anfibio(Veterinario veterinario, Tratador tratador, int id = 0, string classe = "", string nome_cientifico = "", char sexo = '', double tamanho = 0, string dieta = "", string nome_batismo = "", int total_mudas, date ultima_muda){
+	:Animal(Veterinario veterinario, double tamanho, string classe, string nome_cientifico, string nome_batismo, string dieta, char sexo, bool ameacadoExtincao, Tratador tratador);
 	this->total_mudas = total_mudas;
 	this->ultima_muda = ultima_muda;
 };
