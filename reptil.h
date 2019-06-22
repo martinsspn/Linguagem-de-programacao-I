7f7a857716387643552dcac0ce777ce2e99cae3d
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-22 16:45:05 -0300

    Update reptil.h

--- reptil.h
+++ reptil.h
@@ -7,7 +7,7 @@ private:
 	string tipo_veneno;
 
 public:
-	Reptil(bool veneno, string tipo_veneno);
+	Reptil(bool veneno, string tipo_veneno) : Animal(Veterinario veterinario, Tratador tratador, int id, string classe, string nome_cientifico, char sexo, double tamanho, string dieta, string nome_batismo);
 	void setVeneno(bool veneno);
 	bool getVeneno();
 	void setTipo_veneno(string tipo_veneno);
