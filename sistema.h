b3e2b46a44174d905e81ce35cf1ef9d90a1caaec
Gabriel Lucas <47764415+gabriellucasml@users.noreply.github.com>
2019-06-22 02:34:35 -0300

    Implementar a função de adicionar animais
    Incompleta!

--- sistema.h
+++ sistema.h
@@ -7,12 +7,25 @@ private:
 	/**@brief Guarda a seleção do menu*/
 	int selecaoAtual;
 	int selecaoClasse;
-
+	struct Animal{
+		int _id;
+		double _tamanho;
+		string _classe;
+		string _nome_cientifico;
+		string _nome_batismo;
+		string _dieta;
+		char _sexo;
+		bool _ameacadoExtincao;
+		Veterinario _veterinario;
+		Tratador _tratador;
+	} animal;
+	
 public:
 	Sistema();
 	void displayMenu();
 	void displayClasses();
 	void operarSistema();
+	void setStructAnimal();
 	~Sistema();
 };
 
